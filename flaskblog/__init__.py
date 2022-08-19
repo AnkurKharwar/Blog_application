197c7c8a7d04d57782872cf74a1a6550fc6287aa
unknown <ankurkharwar8@gmail.com>
2022-08-19 14:14:46 +0530

    commit

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,7 +10,8 @@ from flask_mail import Mail
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '788be05d80e7f0fed58c823b5e575c00'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://czvkjpyjudekzf:cfd82092307eda9f2eb6c8cc6a6dbecf4f461d5bd8940f00b3eb81def5765a5e@ec2-44-205-64-253.compute-1.amazonaws.com:5432/da3tv3dn03u9m5'
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
